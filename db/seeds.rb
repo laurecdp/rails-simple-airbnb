5813b7e2d49501b9ae0dc133e9e2106ae5f3da7e
Laure Curallucci de Peretti <laurecdp@gmail.com>
2021-02-10 15:46:00 +0100

    Rails new with frontend

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Flat.destroy_all
+puts 'Creating 5 fake flats...'
+5.times do
+  flat = Flat.new(
+    name: "Flat in #{Faker::Books::Dune.city}",
+    address: Faker::Address.full_address,
+    description: Faker::Books::Dune.quote,
+    price_per_night: rand(25..200),
+    number_of_guests: rand(1..10)
+  )
+  flat.save!
+end
+puts 'Finished!'
